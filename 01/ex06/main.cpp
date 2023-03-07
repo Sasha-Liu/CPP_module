fe499d6e02b9cdfde3a1d39f57f15d55749b8c26
Hsin Yu Liu <dewseileaf@gmail.com>
2023-03-07 10:24:19 +0100

    hell

--- 01/ex06/main.cpp
+++ 01/ex06/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 17:34:53 by sasha             #+#    #+#             */
-/*   Updated: 2023/03/06 14:26:33 by hsliu            ###   ########.fr       */
+/*   Updated: 2023/03/07 10:22:38 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,40 +22,40 @@ int main(int argc, char **argv)
 	
 	if (argc != 2)
 	{
-		std::cout << "[ Probably complaining about insignificant problems ]\n";
+		harl.complain("other");
 		return (0);
 	}
 	level = argv[1];
-	filter = -1;
+	filter = 0;
 	if (level.compare("DEBUG") == 0)
-		filter = 0;
-	else if (level.compare("INFO") == 0)
 		filter = 1;
-	else if (level.compare("WARNING") == 0)
+	else if (level.compare("INFO") == 0)
 		filter = 2;
-	else if (level.compare("ERROR") == 0)
+	else if (level.compare("WARNING") == 0)
 		filter = 3;
+	else if (level.compare("ERROR") == 0)
+		filter = 4;
 	switch(filter)
 	{
-		case 0:
+		case 1:
 			harl.complain("DEBUG");
 			harl.complain("INFO");
 			harl.complain("WARNING");
 			harl.complain("ERROR");
 			break ;
-		case 1:
+		case 2:
 			harl.complain("INFO");
 			harl.complain("WARNING");
 			harl.complain("ERROR");
 			break ;
-		case 2:
+		case 3:
 			harl.complain("WARNING");
 			harl.complain("ERROR");
 			break ;
-		case 3:
+		case 4:
 			harl.complain("ERROR");
 			break ;
 		default:
-			std::cout << "[ Probably complaining about insignificant problems ]\n";
+			harl.complain("other");
 	}
 }

5e5f12650876902fcac54739cce51a2ad30407c9
Hsin Yu Liu <dewseileaf@gmail.com>
2023-03-27 16:41:40 +0200

    norm

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 14:59:23 by sasha             #+#    #+#             */
-/*   Updated: 2023/03/03 15:12:52 by sasha            ###   ########.fr       */
+/*   Updated: 2023/03/27 16:40:08 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(void)
 	std::cout << "*stringPTR: " << *stringPTR << std::endl;
 	std::cout << "stringREF : " << stringREF << std::endl;
 
-	std::cout << "\nreassign str to \"HI THIS IS BRIAN\"\n\n";
+	std::cout << "\nreassign \"HI THIS IS BRIAN\" to string\n\n";
 	str = "HI THIS IS BRIAN";
 	
 	std::cout << "Memory address: \n";

9f76541b9432e69498ae5dfa4376f8ceb9b997bc
Hsin Yu Liu <dewseileaf@gmail.com>
2023-04-24 21:00:08 +0200

    change name

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 10:35:37 by sasha             #+#    #+#             */
-/*   Updated: 2023/03/07 11:25:09 by sasha            ###   ########.fr       */
+/*   Updated: 2023/04/24 20:58:56 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,37 @@
 int	Fixed::precision = 8;
 
 Fixed::Fixed(void)
-	:raw(0)
+	:_raw(0)
 {
 	std::cout << "Default constructor called\n";
 }
 
 Fixed::Fixed(Fixed const &fix)
-	:raw(fix.raw)
+	:_raw(fix._raw)
 {
 	std::cout << "Copy constructor called\n";
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Desstructor called\n";
+	std::cout << "Destructor called\n";
 }
 
 Fixed	&Fixed::operator=(Fixed const &fix)
 {
 	std::cout << "Copy assignment operator called\n";
-	this->raw = fix.raw;
+	this->_raw = fix._raw;
 	return (*this);
 }
 
 int		Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called\n";
-	return (this->raw);
+	return (this->_raw);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called\n";
-	this->raw = raw;
+	this->_raw = raw;
 }

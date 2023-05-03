b3ac9eebc790c0e1c6e4cbe3af0e3923dd2ecad8
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-03 18:23:47 +0200

    add prototype

--- 05/ex01/Form.hpp
+++ 05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 18:04:24 by sasha             #+#    #+#             */
-/*   Updated: 2023/05/03 18:11:52 by sasha            ###   ########.fr       */
+/*   Updated: 2023/05/03 18:23:09 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,34 @@
 # include "Bureaucrat.hpp"
 # include <stdexcept>
 # include <iostream>
+# include <string>
 
 class Form
 {
 	public:
+		Form(void);
+		Form(Form const &form);
+		Form(std::string name, int sign_grade, int exec_grade);
+		~Form(void);
 
+		Form				&operator=(Form const &form);
+		std::string const	&getName(void) const;
+		bool				isSigned(void) const;
+		int					getSignGrade(void) const;
+		int					getExecGrade(void) const;
+		void				beSigned(Bureaucrat const &john);
+
+		class	GradeTooHighException;
+		class	GradeTooLowException;
 
 	private:
-		
-
-
-
+		std::string const	_name;
+		bool				_signed;
+		int					_sign_grade;
+		int					_exec_grade;
 };
+
+std::ostream	&operator<<(std::ostream &out, Form const &form);
 
 
 #endif

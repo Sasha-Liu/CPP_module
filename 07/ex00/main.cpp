f7724384afe82d0f3d5249f5e4645278c44bc92a
Hsin-yu Liu <hsliu@z4r12p1.42lyon.fr>
2023-05-08 15:00:25 +0200

    ex00

---
+++ 07/ex00/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 12:50:23 by hsliu             #+#    #+#             */
+/*   Updated: 2023/05/08 14:57:44 by hsliu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
---
+++ 07/ex00/whatever.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 12:51:06 by hsliu             #+#    #+#             */
+/*   Updated: 2023/05/08 14:58:33 by hsliu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template<typename T>
+void	swap(T &a, T &b)
+{
+	T	temp;
+	
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template<typename T>
+T	min(T a, T b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+template<typename T>
+T	max(T a, T b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+# endif

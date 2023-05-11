259725577c75ec8b70c04510589eb7fb37d70d48
Hsin-yu Liu <hsliu@z4r11p7.42lyon.fr>
2023-05-11 15:16:55 +0200

    ex00

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 11:16:39 by hsliu             #+#    #+#             */
-/*   Updated: 2023/05/11 14:00:11 by hsliu            ###   ########.fr       */
+/*   Updated: 2023/05/11 14:38:31 by hsliu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,18 @@ int	main(void)
 {
 	int	tab[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	
-	std::vector<int>		tab1(tab, tab + 10);
-	std::deque<int>			tab2(tab, tab + 10);
-	std::list<int>			tab3(tab, tab + 10);
+	std::vector<int>			tab1(tab, tab + 10);
+	std::deque<int>				tab2(tab, tab + 10);
+	std::list<int>				tab3(tab, tab + 10);
+
+	std::vector<int>::const_iterator	it1 = easyfind(tab1, 0);
+	std::deque<int>::const_iterator		it2 = easyfind(tab2, 1);
+	std::list<int>::const_iterator		it3 = easyfind(tab3, 2);
 	
-	if (easyfind(tab1, 0) != tab1.end())
-		std::cout << *(easyfind(tab1, 9)) << std::endl;
-	if (easyfind(tab2, 1) != tab2.end())
-		std::cout << *(easyfind(tab2, 8)) << std::endl;
-	if (easyfind(tab3, 2) != tab3.end())
-		std::cout << *(easyfind(tab3, 7)) << std::endl;
-	
+	if (it1 != tab1.end())
+		std::cout << *it1 << std::endl;
+	if (it2 != tab2.end())
+		std::cout << *it2 << std::endl;
+	if (it3 != tab3.end())
+		std::cout << *it3 << std::endl;
 }

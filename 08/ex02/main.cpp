4e283b57ab130a65800392861d426f97a61e63d0
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-12 12:04:06 +0200

    all good

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -6,20 +6,65 @@
 /*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 21:52:53 by sasha             #+#    #+#             */
-/*   Updated: 2023/05/12 11:19:19 by sasha            ###   ########.fr       */
+/*   Updated: 2023/05/12 12:03:35 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
+#include <stack>
 
-int	main(void)
+int main()
 {
-	MutantStack<int>	stack1;
-	MutantStack<int>	stack2;
-	
-
-	stack1.push(10);
-	stack2.push(20);
-	std::cout << "stack1 == stack2 returns " << (stack1 == stack2) << std::endl;
+	/*	test in project	*/
+	std::cout << "test in project: mutant stack" << std::endl;
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int, MutantStack<int> > s(mstack);
+	}
+	/*	test in project	*/
+	std::cout << "test in project: list" << std::endl;
+	{
+		std::list<int> mlist;
+		mlist.push_back(5);
+		mlist.push_back(17);
+		std::cout << mlist.back() << std::endl;
+		mlist.pop_back();
+		std::cout << mlist.size() << std::endl;
+		mlist.push_back(3);
+		mlist.push_back(5);
+		mlist.push_back(737);
+		//[...]
+		mlist.push_back(0);
+		std::list<int>::iterator it = mlist.begin();
+		std::list<int>::iterator ite = mlist.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int, std::list<int> > s(mlist);
+	}
 }

f61e3ffa6e60f98950a4d76ca8a540e241c97072
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-15 15:58:54 +0200

    negative

--- 09/ex01/RPN.cpp
+++ 09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 14:52:32 by sasha             #+#    #+#             */
-/*   Updated: 2023/05/13 20:23:18 by sasha            ###   ########.fr       */
+/*   Updated: 2023/05/15 15:57:58 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,25 @@ RPN::~RPN(void) {}
 	
 bool	RPN::calculator(std::string const &str)
 {
-	std::stack<int>	stack;
-	std::stringstream	input(str);
-	char				token;
+	std::stack<int>		stack;
 
-	while (input >> token)
+	for (std::string::const_iterator i = str.begin(); i != str.end(); i++)
 	{
-		if (isdigit(token))
-			stack.push(static_cast<int>(token - '0'));
-		else if (stack.size() != 2 || !doMath(token, stack))
+		if (*i == ' ')
+			;
+		else if (isdigit(*i))
+			stack.push(static_cast<int>(*i - '0'));
+		else if (stack.size() < 2 && *i == '-' && isdigit(*(i + 1)))
+		{
+			i++;
+			stack.push(static_cast<int>('0' - *i));
+		}	
+		else if (stack.size() == 2)
+		{
+			if (!doMath(*i, stack))
+				return (false);
+		}
+		else
 			return (false);
 	}
 	if (stack.size() != 1)
